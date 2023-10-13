252c5af3d8598e36c4aec9e8d0abd152cbafed0a
MM1212 <mm.jogos.game@gmail.com>
2023-10-13 16:52:55 +0100

    fix(server/auth): temporarily set the user as logged in so that auth.updateNewToken correctly works (fallback if intra & db fetch fails)

--- src/server/typings/auth/session/user.ts
+++ src/server/typings/auth/session/user.ts
@@ -7,6 +7,6 @@ export interface IAuthSessionLoggedUser extends IUser {
 }
 
 export type IAuthSessionUser = IAuthSessionLoggedUser | { loggedIn: false };
-export type IAuthSessionUserData = IAuthSessionLoggedUser & {
+export type IAuthSessionUserData = Omit<IAuthSessionLoggedUser, 'loggedIn'> & {
   loggedIn: boolean;
 };
