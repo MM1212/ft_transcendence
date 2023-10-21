0242863a7f401fdf9b1688da0ee18459010508ba
MM1212 <mm.jogos.game@gmail.com>
2023-10-21 22:55:50 +0100

    fix(package.json): fucking fastify version missmatching was breaking type injection in relevant files...

--- src/server/typings/session.d.ts
+++ src/server/typings/session.d.ts
@@ -1,26 +1,5 @@
-import { Session, SessionData } from '@fastify/secure-session';
-import { FastifyRequest, FastifyInstance } from 'fastify';
+import { SessionData } from '@fastify/secure-session';
 import { IAuthSession } from '@typings/auth/session';
-
-declare module 'fastify' {
-  interface FastifyInstance {
-    createSecureSession(
-      data?: Record<string, any>,
-    ): fastifySecureSession.Session;
-    decodeSecureSession(
-      cookie: string,
-      log?: FastifyBaseLogger,
-      sessionName?: string,
-    ): fastifySecureSession.Session | null;
-    encodeSecureSession(
-      session: fastifySecureSession.Session,
-      sessionName?: string,
-    ): string;
-  }
-  interface FastifyRequest {
-    session: Session;
-  }
-}
 
 declare module '@fastify/secure-session' {
   interface SessionData extends IAuthSession {}
