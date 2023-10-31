5fd1e513e30b04fad4bb2ef67cffec063d5e50a0
Hello <you@example.com>
2023-10-31 20:47:06 +0000

    added some achievements. Do I need to wrap each item in a Grid component?

--- src/client/src/views/lobby/components/user/UserAchievements.tsx
+++ src/client/src/views/lobby/components/user/UserAchievements.tsx
@@ -1,15 +1,46 @@
-import { Chip, Typography } from "@mui/joy";
+import { Chip, Grid, Typography } from "@mui/joy";
 
 export default function UserAchievements() {
     return (<>
         <Typography level="h2">
             Achievements
         </Typography>
-        <Chip startDecorator="☀️">
-            Spent an entire day in-game
-        </Chip>
-        <Chip startDecorator="🏖️">
-            Spent 4 hours in the lobby without entering a game
-        </Chip>
+        <Grid
+            container
+            spacing={1}
+            // columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+            sx={{ width: '80%', flexGrow: 1 }}
+        >
+            <Grid xs="auto">
+                <Chip startDecorator="☀️">
+                    Spent an entire day in-game
+                </Chip>
+            </Grid>
+            <Grid xs="auto">
+                <Chip startDecorator="🏖️">
+                    Spent 4 hours in the lobby without entering a game
+                </Chip>
+            </Grid>
+            <Grid xs="auto">
+                <Chip startDecorator="🫠">
+                    16 games losing-streak
+                </Chip>
+            </Grid>
+            <Grid xs="auto">
+                <Chip startDecorator="🔥">
+                    8 game winning-streak
+                </Chip>
+            </Grid>
+            <Grid>
+                <Chip startDecorator="🫂">
+                    Spectate your friends losing 20 times
+                </Chip>
+            </Grid>
+            <Grid>
+                <Chip startDecorator="🧑‍🤝‍🧑">
+                    Spectate 5 games in a row
+                </Chip>
+            </Grid>
+        </Grid>
     </>);
 }
