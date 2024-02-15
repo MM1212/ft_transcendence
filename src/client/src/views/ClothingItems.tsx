5d32796e9f4ce6e4cbf05ba8460e2639b1a7bf42
MM1212 <mm.jogos.game@gmail.com>
2024-02-15 17:24:06 +0000

    feat(client/dev/clothing): add to shop form

--- src/client/src/views/ClothingItems.tsx
+++ src/client/src/views/ClothingItems.tsx
@@ -1,30 +1,71 @@
 import GenericPlaceholder from '@components/GenericPlaceholder';
 import Pagination from '@components/Pagination';
+import AlertIcon from '@components/icons/AlertIcon';
+import BoxingGloveIcon from '@components/icons/BoxingGloveIcon';
 import BugIcon from '@components/icons/BugIcon';
 import CartIcon from '@components/icons/CartIcon';
+import CartPlusIcon from '@components/icons/CartPlusIcon';
+import CurrencyTwdIcon from '@components/icons/CurrencyTwdIcon';
 import FilterIcon from '@components/icons/FilterIcon';
+import FormatColorFillIcon from '@components/icons/FormatColorFillIcon';
+import ImageTextIcon from '@components/icons/ImageTextIcon';
+import LabelIcon from '@components/icons/LabelIcon';
 import MagnifyIcon from '@components/icons/MagnifyIcon';
+import SafetyGogglesIcon from '@components/icons/SafetyGogglesIcon';
+import ShapeIcon from '@components/icons/ShapeIcon';
+import ShoeSneakerIcon from '@components/icons/ShoeSneakerIcon';
+import TshirtCrewIcon from '@components/icons/TshirtCrewIcon';
+import WizardHatIcon from '@components/icons/WizardHatIcon';
 import MenuOption from '@components/menu/MenuOption';
 import { useDebounce, useDebouncedValue } from '@hooks/lodash';
 import { buildTunnelEndpoint, jsonFetcher } from '@hooks/tunnel';
+import { useModal, useModalActions } from '@hooks/useModal';
+import { Button, Grid, ModalClose } from '@mui/joy';
 import {
   Box,
   Checkbox,
   Chip,
   CircularProgress,
+  DialogContent,
+  DialogTitle,
   Divider,
   Dropdown,
+  FormControl,
+  FormLabel,
   IconButton,
   Input,
   Menu,
   MenuButton,
+  Modal,
+  ModalDialog,
+  Option,
+  Select,
   Sheet,
   Stack,
+  Tooltip,
   Typography,
+  type SvgIconProps,
 } from '@mui/joy';
+import { alpha } from '@theme';
 import type { ClothingItem } from '@typings/lobby/dev/clothing';
+import LobbyModel from '@typings/models/lobby';
+import { randomInt } from '@utils/random';
 import React from 'react';
+import { atom } from 'recoil';
 import useSWR from 'swr';
+
+interface DevClothingListModalState {
+  item: ClothingItem;
+  backItem: boolean;
+}
+
+const DEV_CLOTHING_LIST_MODAL_ID = 'lobby:clothing:add-to-shop';
+
+const useDevClothingListModal = () =>
+  useModal<DevClothingListModalState>(DEV_CLOTHING_LIST_MODAL_ID);
+
+const useDevClothingListModalActions = () =>
+  useModalActions<DevClothingListModalState>(DEV_CLOTHING_LIST_MODAL_ID);
 
 interface Filter {
   query: string;
@@ -37,9 +78,334 @@ const getIconAssetPath = (id: number) =>
     import.meta.env.FRONTEND_PUBLIC_CDN_URL
   }/client/dist/assets/media/clothing/icon/${id}.webp`;
 
+const priceTableCap: Record<
+  LobbyModel.Models.InventoryCategory,
+  [number, number]
+> = {
+  head: [100, 1000],
+  face: [100, 1000],
+  neck: [100, 1000],
+  body: [100, 1000],
+  hand: [100, 1000],
+  feet: [100, 1000],
+  color: [100, 1000],
+};
+
+const categories: {
+  value: LobbyModel.Models.InventoryCategory;
+  label: string;
+  icon: React.ComponentType<SvgIconProps>;
+}[] = [
+  { value: 'head', label: 'Head', icon: WizardHatIcon },
+  { value: 'body', label: 'Body', icon: TshirtCrewIcon },
+  { value: 'hand', label: 'Hand', icon: BoxingGloveIcon },
+  { value: 'feet', label: 'Feet', icon: ShoeSneakerIcon },
+  { value: 'face', label: 'Face', icon: SafetyGogglesIcon },
+  { value: 'color', label: 'Color', icon: FormatColorFillIcon },
+];
+
+function AddClothingToShopModalForm(): JSX.Element {
+  const { data, setData } = useDevClothingListModal();
+  const [itemName, setItemName] = React.useState(data.item.name);
+  const [itemDescription, setItemDescription] = React.useState('');
+  const [itemCategory, setItemCategory] =
+    React.useState<LobbyModel.Models.InventoryCategory>('color');
+
+  const computeNewPrice = (category: LobbyModel.Models.InventoryCategory) =>
+    randomInt(...priceTableCap[category]).toString();
+  const [itemPrice, setItemPrice] = React.useState<string>(() =>
+    computeNewPrice(itemCategory)
+  );
+
+  React.useEffect(() => {
+    setItemName(data.item.name);
+    setItemDescription('');
+    setItemCategory('color');
+    setItemPrice(computeNewPrice('color'));
+  }, [data.item]);
+
+  React.useEffect(() => {
+    setItemPrice(computeNewPrice(itemCategory));
+  }, [itemCategory]);
+
+  return (
+    <Box gap={2} display="flex" flexDirection="column" mt={2} flexGrow={1}>
+      <Stack direction="row" spacing={2}>
+        <Typography level="title-md">
+          Item: <Typography level="body-lg">{itemName}</Typography>
+        </Typography>
+      </Stack>
+      <FormControl required>
+        <FormLabel>Item Name</FormLabel>
+        <Input
+          value={itemName}
+          onChange={(e) => setItemName(e.target.value)}
+          placeholder="Item's name"
+          color="neutral"
+          startDecorator={<LabelIcon />}
+        />
+      </FormControl>
+      <FormControl required>
+        <FormLabel>Item Description</FormLabel>
+        <Input
+          value={itemDescription}
+          onChange={(e) => setItemDescription(e.target.value)}
+          placeholder="Item's description"
+          color="neutral"
+          startDecorator={<ImageTextIcon />}
+        />
+      </FormControl>
+      <Stack direction="row" spacing={1} alignItems="center">
+        <FormControl required sx={{ width: '50%' }}>
+          <FormLabel>Category</FormLabel>
+          <Select
+            value={itemCategory}
+            onChange={(e, value) => value && setItemCategory(value)}
+            color="neutral"
+            startDecorator={<ShapeIcon />}
+          >
+            {categories.map(({ value, label, icon: Icon }) => (
+              <Option
+                value={value}
+                key={value}
+                label={
+                  <Stack direction="row" spacing={0.5} alignItems="center">
+                    <Icon size="md" />
+                    <Typography level="body-md" component="div">
+                      {label}
+                    </Typography>
+                  </Stack>
+                }
+              >
+                <Stack direction="row" spacing={0.5} alignItems="center">
+                  <Icon size="md" />
+                  <Typography level="body-md" component="div">
+                    {label}
+                  </Typography>
+                </Stack>
+              </Option>
+            ))}
+          </Select>
+        </FormControl>
+        <FormControl sx={{ width: '50%' }}>
+          <FormLabel>Back Item Sprite</FormLabel>
+          <Checkbox
+            checked={data.backItem}
+            onChange={(e) => setData({ ...data, backItem: !data.backItem })}
+            label="Is back item?"
+          />
+        </FormControl>
+      </Stack>
+      <FormControl
+        required
+        error={isNaN(parseInt(itemPrice)) || parseInt(itemPrice) < 0}
+      >
+        <FormLabel>Price</FormLabel>
+        <Input
+          type="number"
+          value={itemPrice}
+          onChange={(e) => setItemPrice(e.target.value)}
+          placeholder="Item's price"
+          color="neutral"
+          startDecorator={<CurrencyTwdIcon />}
+        />
+      </FormControl>
+      <Button color="primary" sx={{ mt: 3 }}>
+        Submit
+      </Button>
+    </Box>
+  );
+}
+
+function AddClothingToShopModalPreview(): JSX.Element {
+  const { data } = useDevClothingListModal();
+  const toRenderImgs = [
+    [
+      `${
+        import.meta.env.FRONTEND_PUBLIC_CDN_URL
+      }/client/dist/assets/media/clothing/icon/${data.item.id}.webp`,
+      'icon',
+    ],
+    [
+      `${
+        import.meta.env.FRONTEND_PUBLIC_CDN_URL
+      }/client/dist/assets/media/clothing/paper/${data.item.id}.webp`,
+      'paper',
+    ],
+    [
+      `${
+        import.meta.env.FRONTEND_PUBLIC_CDN_URL
+      }/client/dist/assets/media/clothing/sprites/${data.item.id}-0.webp`,
+      'sprites',
+    ],
+    [
+      `${
+        import.meta.env.FRONTEND_PUBLIC_CDN_URL
+      }/client/dist/assets/media/clothing/paper/${data.item.id}_back.webp`,
+      'paper back',
+    ],
+  ];
+
+  return (
+    <Grid container spacing={2}>
+      {toRenderImgs.map(([url, label]) => (
+        <Grid key={label} xs={6}>
+          <Sheet
+            variant="outlined"
+            sx={{
+              p: 1,
+              borderRadius: 'md',
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              justifyContent: 'center',
+              gap: 1,
+              bgcolor: 'background.level1',
+              width: '100%',
+            }}
+          >
+            <img
+              src={url}
+              alt={label}
+              style={{
+                width: '100%',
+                aspectRatio: '1 / 1',
+                objectFit: 'scale-down',
+              }}
+              loading="lazy"
+            />
+            <Typography level="body-md" textAlign="center">
+              {label}
+            </Typography>
+          </Sheet>
+        </Grid>
+      ))}
+    </Grid>
+  );
+}
+
+function AddClothingToShopModal(): JSX.Element {
+  const { isOpened, close, data } = useDevClothingListModal();
+  return (
+    <Modal open={isOpened} onClose={close}>
+      <ModalDialog minWidth="md" maxWidth="lg">
+        <ModalClose />
+        <DialogTitle>Add to shop</DialogTitle>
+        <DialogContent
+          sx={{
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'space-around',
+            gap: 1,
+            overflow: 'hidden',
+            position: 'relative',
+          }}
+        >
+          <Box width="45%" height="100%">
+            <AddClothingToShopModalForm />
+          </Box>
+          <Divider orientation="vertical" />
+          <Box width="45%">
+            <AddClothingToShopModalPreview />
+          </Box>
+        </DialogContent>
+      </ModalDialog>
+    </Modal>
+  );
+}
+
+function ItemEntryWarnings(item: ClothingItem) {
+  const warnings = [];
+  if (!item.props.icon) warnings.push('Icon missing');
+  if (!item.props.paper) warnings.push('Paper missing');
+  if (!item.props.sprites) warnings.push('Sprites missing');
+  if (warnings.length === 0) return null;
+  return (
+    <Tooltip
+      title={
+        <Stack direction="row" gap={0.5}>
+          {warnings.map((warning) => (
+            <Chip
+              key={warning}
+              variant="soft"
+              color="danger"
+              startDecorator={<AlertIcon />}
+            >
+              {warning}
+            </Chip>
+          ))}
+        </Stack>
+      }
+      size="lg"
+    >
+      <Sheet
+        sx={{
+          // position: 'absolute',
+          // top: 0,
+          // right: 0,
+          // mt: '-14%',
+          // mr: '-14%',
+          px: 0.75,
+          borderRadius: 'xl',
+          border: '2px solid',
+          borderColor: 'divider',
+          bgcolor: 'background.surface',
+        }}
+        variant="outlined"
+      >
+        <AlertIcon color="warning" size="xs" />
+      </Sheet>
+    </Tooltip>
+  );
+}
+
+function ItemEntryAddToShop({
+  item,
+  hovered,
+}: {
+  item: ClothingItem;
+  hovered: boolean;
+}) {
+  const { open } = useDevClothingListModalActions();
+  if (!hovered) return null;
+  return (
+    <Sheet
+      sx={{
+        width: '100%',
+        height: '100%',
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        border: '2px solid',
+        borderColor: 'divider',
+        backdropFilter: 'blur(2px)',
+        bgcolor: (theme) =>
+          alpha(theme.resolveVar('palette-background-level2'), 0.6),
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+      variant="outlined"
+    >
+      <Tooltip title="Add to shop">
+        <IconButton
+          size="lg"
+          color="success"
+          variant="plain"
+          sx={{ p: 0.5, m: 0.5, borderRadius: 'lg' }}
+          onClick={() => open({ item, backItem: false })}
+        >
+          <CartPlusIcon />
+        </IconButton>
+      </Tooltip>
+    </Sheet>
+  );
+}
+
 const ItemEntry = React.memo(function ItemEntry(
   item: ClothingItem
 ): JSX.Element {
+  const [hovered, setHovered] = React.useState(false);
   return (
     <Sheet
       key={item.id}
@@ -55,6 +421,7 @@ const ItemEntry = React.memo(function ItemEntry(
         bgcolor: 'background.level1',
         flex: '0 0 14dvh',
         flexGrow: 0,
+        position: 'relative',
       }}
     >
       <Box
@@ -70,25 +437,35 @@ const ItemEntry = React.memo(function ItemEntry(
           display: 'flex',
           justifyContent: 'center',
           alignItems: 'center',
+          position: 'relative',
+          overflow: 'hidden',
         }}
+        onMouseEnter={() => setHovered(true)}
+        onMouseLeave={() => setHovered(false)}
       >
         <img
           src={getIconAssetPath(item.id)}
           alt={item.name}
           style={{
             width: '100%',
-            height: '100%',
+            aspectRatio: '1 / 1',
             objectFit: 'scale-down',
           }}
           loading="lazy"
         />
+        <ItemEntryAddToShop item={item} hovered={hovered} />
       </Box>
-      <Typography level="body-sm" textAlign="center" width="10dvh" noWrap>
-        {item.name}
-      </Typography>
-      <Typography level="body-xs" textAlign="center">
-        (ID: {item.id})
-      </Typography>
+      <Tooltip enterDelay={1000} title={item.name}>
+        <Typography level="body-sm" textAlign="center" width="10dvh" noWrap>
+          {item.name}
+        </Typography>
+      </Tooltip>
+      <Stack direction="row" alignItems="center" spacing={1}>
+        <Typography level="body-xs" textAlign="center">
+          (ID: {item.id})
+        </Typography>
+        <ItemEntryWarnings {...item} />
+      </Stack>
     </Sheet>
   );
 });
@@ -158,9 +535,20 @@ function Header({
                       showAddedToShop: !prev.showAddedToShop,
                     }));
                   }}
-                  selected={filter.showAddedToShop}
                 >
                   <Typography level="body-sm">Show added to shop</Typography>
+                  <Checkbox
+                    color="success"
+                    variant="plain"
+                    checked={filter.showAddedToShop}
+                    onChange={(e) => {
+                      e.stopPropagation();
+                      setFilter((prev) => ({
+                        ...prev,
+                        showAddedToShop: e.target.checked,
+                      }));
+                    }}
+                  />
                 </MenuOption>
                 <MenuOption
                   icon={BugIcon}
@@ -171,9 +559,20 @@ function Header({
                       showProbablyBroken: !prev.showProbablyBroken,
                     }));
                   }}
-                  selected={filter.showProbablyBroken}
                 >
                   <Typography level="body-sm">Show probably broken</Typography>
+                  <Checkbox
+                    color="success"
+                    variant="plain"
+                    checked={filter.showProbablyBroken}
+                    onChange={(e) => {
+                      e.stopPropagation();
+                      setFilter((prev) => ({
+                        ...prev,
+                        showProbablyBroken: e.target.checked,
+                      }));
+                    }}
+                  />
                 </MenuOption>
               </Menu>
             </Dropdown>
@@ -202,13 +601,12 @@ function ClothingItemEntries(): JSX.Element {
     showProbablyBroken: true,
   });
   const filteredItems = React.useMemo(() => {
-    console.log('filterItems', clothingItems, filter);
-
     if (!clothingItems) return [];
     const filtered = clothingItems.filter((item) => {
       if (filter.query.trim().length > 0) {
         if (!item.name.toLowerCase().includes(filter.query.toLowerCase()))
           return false;
+        if (String(item.id).includes(filter.query)) return false;
       }
       if (!filter.showAddedToShop && item.in_shop) return false;
       if (!filter.showProbablyBroken) {
@@ -217,8 +615,6 @@ function ClothingItemEntries(): JSX.Element {
       }
       return true;
     });
-    console.log('filtered', filtered.length);
-
     return filtered;
   }, [clothingItems, filter]);
   const items = React.useMemo(
@@ -257,13 +653,12 @@ function ClothingItemEntries(): JSX.Element {
         filter={filter}
         setFilter={setFilter}
       />
-      <Box flexGrow={1} overflow="auto">
+      <Box flexGrow={1} overflow="auto" pr={0.5}>
         <Box
-          display="flex"
+          display="grid"
           width="100%"
-          flexWrap={'wrap'}
-          justifyContent="flex-start"
-          alignItems="flex-start"
+          gridTemplateColumns={`repeat(auto-fill, minmax(14dvh, 1fr))`}
+          gridAutoRows={`minmax(14dvh, 1fr)`}
           gap={1}
         >
           {items.map((item) => (
@@ -301,6 +696,7 @@ export default function ClothingItemsDev() {
       }}
     >
       <ClothingItemEntries />
+      <AddClothingToShopModal />
     </Sheet>
   );
 }
